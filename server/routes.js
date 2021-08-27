e8b83fc986966672b8a73c2cff2da9a21497b4d6
Simon Si <simonsi@outlook.com>
2021-08-26 23:44:12 -0700

    Implemented Redis

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 const dbAction = require('../db/dbactions.js')
 
-router.get('/reviews', dbAction.getReviews)
-router.get('/reviews/meta', dbAction.getMeta)
+router.get('/reviews', dbAction.cache, dbAction.getReviews)
+router.get('/reviews/meta', dbAction.cache, dbAction.getMeta)
 router.post('/reviews', dbAction.postReviews)
 router.put('/reviews/:review_id/helpful', dbAction.updateHelpfulness)
 router.put('/reviews/:review_id/report', dbAction.reported)
